9203a97a2efc6a81b74f2be91aa35aba58f444aa
Link <50463727+link-discord@users.noreply.github.com>
2024-08-31 23:08:56 +0200

    Revert "Change the svelte page to use +page.server.ts to load data instead of an api route"
    This reverts commit fd2dc5a2c6885f1a44e1ef83a79e5fdfad50695a.

---
+++ src/routes/api/avatar/+server.ts
@@ -0,0 +1,28 @@
+import { error } from '@sveltejs/kit'
+import { json } from '@sveltejs/kit'
+
+export const GET = async ({ url }) => {
+	const id = String(url.searchParams.get('id'))
+
+	if (!id) {
+		return error(400, 'Invalid request')
+	}
+
+	const data = await fetch(`https://discord.com/api/v10/users/${id}`, {
+		headers: {
+			Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+			'Content-Type': 'application/json'
+		}
+	}).then((res) => res.json())
+
+    const avatarExtension = data.avatar.startsWith('a_') ? 'gif' : 'webp'
+
+    data.avatar_url = `https://cdn.discordapp.com/avatars/${id}/${data.avatar}.${avatarExtension}?size=4096`
+
+	if (data.banner) {
+		const bannerExtension = data.banner.startsWith('a_') ? 'gif' : 'webp'
+		data.banner_url = `https://cdn.discordapp.com/banners/${id}/${data.banner}.${bannerExtension}?size=4096`
+	}
+
+    return json(data)
+}
