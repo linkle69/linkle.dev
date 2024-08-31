648be910de7cbbf2b94a8e46016a47cacd18d745
link-discord <50463727+link-discord@users.noreply.github.com>
2024-08-31 20:32:09 +0200

    Fix API Route

--- src/routes/api/avatar/+server.ts
+++ src/routes/api/avatar/+server.ts
@@ -16,10 +16,13 @@ export const GET = async ({ url }) => {
 	}).then((res) => res.json())
 
     const avatarExtension = data.avatar.startsWith('a_') ? 'gif' : 'webp'
-    const bannerExtension = data.banner.startsWith('a_') ? 'gif' : 'webp'
 
     data.avatar_url = `https://cdn.discordapp.com/avatars/${id}/${data.avatar}.${avatarExtension}?size=4096`
-    data.banner_url = `https://cdn.discordapp.com/banners/${id}/${data.banner}.${bannerExtension}?size=4096`
+
+	if (data.banner) {
+		const bannerExtension = data.banner.startsWith('a_') ? 'gif' : 'webp'
+		data.banner_url = `https://cdn.discordapp.com/banners/${id}/${data.banner}.${bannerExtension}?size=4096`
+	}
 
     return json(data)
 }
