b1f838490636cab0d428cfc563f3504d5864f4b4
Link <50463727+link-discord@users.noreply.github.com>
2024-12-30 18:05:24 +0100

    Refactor to use Elysia framework

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import express from 'express'
-import cors from 'cors'
+import { Elysia } from 'elysia'
+import { cors } from '@elysiajs/cors'
+import { staticPlugin } from '@elysiajs/static'
+import redis, { commandOptions } from 'redis'
 import fs from 'fs/promises'
 import * as cheerio from 'cheerio'
-import redis, { commandOptions } from 'redis'
 
 const ttl = process.env.TTL ? parseInt(process.env.TTL) : 5 * 60
 const port = process.env.PORT || 3000
@@ -13,7 +14,6 @@ if (!process.env.DISCORD_TOKEN) {
 }
 
 const client = redis.createClient({ url: 'redis://redis' })
-const app = express()
 const id = '476662199872651264'
 
 await client.connect()
@@ -38,9 +38,18 @@ async function fetchUser(id: string) {
     return user
 }
 
+const app = new Elysia()
+
 app.use(cors({ origin: 'https://linkdiscord.xyz' }))
 
-app.get('/', async (req, res) => {
+app.use(
+    staticPlugin({
+        assets: 'src/public',
+        prefix: '/'
+    })
+)
+
+app.get('/', async ({ set }) => {
     const [user, file] = await Promise.all([
         fetchUser(id),
         fs.readFile('src/public/index.html', 'utf8')
@@ -51,12 +60,11 @@ app.get('/', async (req, res) => {
 
     $('head').append(`<meta name="theme-color" content="#${accent_color}">`)
 
-    res.send($.html())
+    set.headers['Content-Type'] = 'text/html'
+    return $.html()
 })
 
-app.use(express.static('src/public', { extensions: ['css', 'js'] }))
-
-app.get('/favicon.png', async (req, res) => {
+app.get('/favicon.png', async ({ set }) => {
     const user = await fetchUser(id)
     let buffer = await client.get(commandOptions({ returnBuffers: true }), 'favicon')
 
@@ -66,7 +74,7 @@ app.get('/favicon.png', async (req, res) => {
         )
 
         if (!data.ok) {
-            res.sendStatus(500)
+            set.status = 500
             return
         }
 
@@ -74,37 +82,28 @@ app.get('/favicon.png', async (req, res) => {
         await client.set('favicon', buffer, { EX: ttl })
     }
 
-    res.contentType('image/png')
-    res.send(buffer)
+    set.headers['Content-Type'] = 'image/png'
+    return buffer
 })
 
-app.get('/avatar/:id', async (req, res) => {
-    const id = req.params.id
-    const size = req.query.size || 128
-    const user = await fetchUser(id)
+app.get('/avatar/:id', async ({ params, query, set }) => {
+    const user = await fetchUser(params.id)
+    const size = query.size || 128
     const is_animated = user.avatar.startsWith('a_')
     const extension = is_animated ? 'gif' : 'webp'
 
-    res.contentType(`image/${extension}`)
-    res.redirect(
-        `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.${extension}?size=${size}`
-    )
+    set.headers['Content-Type'] = `image/${extension}`
+    set.status = 302
+    set.headers.Location = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.${extension}?size=${size}`
 })
 
-app.get('/color/:id', async (req, res) => {
-    const id = req.params.id
-    const user = await fetchUser(id)
+app.get('/color/:id', async ({ params }) => {
+    const user = await fetchUser(params.id)
     const accent_color = user.accent_color.toString(16).padStart(6, '0')
-
-    res.send(`#${accent_color}`)
+    return `#${accent_color}`
 })
 
-app.get('/user/:id', async (req, res) => {
-    const id = req.params.id
-    const user = await fetchUser(id)
-
-    res.json(user)
-})
+app.get('/user/:id', ({ params }) => fetchUser(params.id))
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
